2b8ee397a547972175db96c2a47379c1380f1d71
john-v77 <ionut_vasile85@hotmail.com>
2021-09-27 18:00:02 -0400

    material-ui initial layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Map from './components/Map/Map';
 import List from './components/List/List';
 import PlaceDetails from './components/PlaceDetails/PlaceDetails';
+import {CssBaseline, Grid} from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello!</h1>
-        <Header/>
-        <Map/>
-        <List/>
-        <PlaceDetails/>
-    </div>
+    <>
+        <CssBaseline>
+            <h1>Hello!</h1>
+            <Header/>
+            <Grid container spacing={3} style={{width:'100%'}}>
+                <Grid item xs={12} md={4}>
+                    <List/>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map/>
+                </Grid>
+            </Grid>
+            {/* <PlaceDetails/> */}
+        </CssBaseline>
+    </>
   );
 }
 
