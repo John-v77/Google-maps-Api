a87e45df80fb37c88b08309a148bf53b93bc2505
john-v77 <ionut_vasile85@hotmail.com>
2021-09-27 18:22:51 -0400

    header layout and style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import {Autocomplete} from '@react-google-maps/api';
+import {AppBar, Toolbar, Typography, InputBase, Box, Tooltip} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search';
+import useStyles from './styles';
 
 function Header(props) {
+    const classes = useStyles();
     return (
-        <div>
-            
-        </div>
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography varian='h5'className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display='flex'>
+                    <Typography varian='h6'className={classes.title}>
+                        Explore new places
+                    </Typography>
+                    {/* <Autocomplete> */}
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search ..." classes={{ root: classes.InputRoot, input: classes.inputInput}} />
+                        </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
 
